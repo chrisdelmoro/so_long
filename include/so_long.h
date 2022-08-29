c32409924c7a21c6e758c6de56b3a9446cb15b18
ccamargo <ccamargo@student.42sp.org.br>
2022-08-29 19:11:11 -0300

    Added header file.

--- include/so_long.h
+++ include/so_long.h
@@ -6,9 +6,45 @@
 /*   By: ccamargo <ccamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 20:22:37 by ccamargo          #+#    #+#             */
-/*   Updated: 2022/08/24 22:42:50 by ccamargo         ###   ########.fr       */
+/*   Updated: 2022/08/29 17:27:22 by ccamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+/* MiniLibX Library */
+# include <mlx.h>
+
+/* Xlib keycode - keysymbol mapping, events and masks */
+# include <X11/keysym.h>
+# include <X11/X.h>
+
+/* Libft */
+# include <libft.h>
+# include <ft_printf.h>
+
+/* open() function includes */
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+/* read() and close() include */
+# include <unistd.h>
+
+
+
+/* Constants */
+
+# define WINDOW_WIDTH 800
+# define WINDOW_HEIGHT 600
+# define RED_PIXEL 0x00FF0000
+# define GREEN_PIXEL 0x0000FF00
+# define BLUE_PIXEL 0x000000FF
+
+
+
+/* Structs */
 
 typedef struct	s_img
 {
@@ -26,8 +62,4 @@ typedef struct	s_window
 	t_img	img;
 }				t_window;
 
-# define WINDOW_WIDTH 800
-# define WINDOW_HEIGHT 600
-# define RED_PIXEL 0x00FF0000
-# define GREEN_PIXEL 0x0000FF00
-# define BLUE_PIXEL 0x000000FF
+#endif
