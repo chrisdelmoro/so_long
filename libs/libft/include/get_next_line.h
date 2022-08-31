6b4b44fb8926cbe91b790e4ddff3b02769a0ad94
ccamargo <ccamargo@student.42sp.org.br>
2022-08-30 21:27:31 -0300

    Added get_next_line() function to libft.

---
+++ libs/libft/include/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccamargo <ccamargo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 20:51:16 by ccamargo          #+#    #+#             */
+/*   Updated: 2022/08/30 20:47:41 by ccamargo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <libft.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+/**
+ * @brief The get_next_line() function allocates (with malloc(3)) and returns a
+ * new string, which is the current found line of the opened file.
+ *
+ * @param fd The file descriptor of the opened file.
+ * 
+ * @return (char *) Returns found line or NULL file ended or error occured.
+ */
+char	*get_next_line(int fd);
+
+#endif
---
+++ libs/libft/src/get_next_line.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccamargo <ccamargo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 00:08:30 by ccamargo          #+#    #+#             */
+/*   Updated: 2022/08/30 20:29:15 by ccamargo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <get_next_line.h>
+
+static void	freethis(char **str, void *content)
+{
+	if (*str)
+	{
+		free(*str);
+		*str = (char *) content;
+	}
+}
+
+static int	feed_accumulator(char **accumulator, int fd)
+{
+	char	*tmp;
+	char	*buffer;
+	int		read_count;
+
+	buffer = (char *) malloc(BUFFER_SIZE + 1 * sizeof(char));
+	read_count = read(fd, buffer, BUFFER_SIZE);
+	if (read_count <= 0)
+	{
+		freethis(&buffer, NULL);
+		return (read_count);
+	}
+	buffer[read_count] = '\0';
+	tmp = *accumulator;
+	*accumulator = ft_strjoin(tmp, buffer);
+	if (ft_strlen(tmp))
+		freethis(&tmp, NULL);
+	freethis(&buffer, NULL);
+	return (read_count);
+}
+
+static void	close_line_n(char **accumulator, char **line, int read_count)
+{
+	char	*tmp;
+	size_t	i;
+
+	i = 0;
+	while ((*accumulator)[i] != '\n' && (*accumulator)[i])
+		i++;
+	*line = ft_substr(*accumulator, 0, i + 1);
+	tmp = *accumulator;
+	if (read_count > 0)
+		*accumulator = ft_substr(tmp, i + 1, ft_strlen(tmp));
+	freethis(&tmp, NULL);
+}
+
+static int	close_gnl(char **accumulator, char **line, int read_count)
+{
+	if (ft_strchr(*accumulator, '\n'))
+	{
+		close_line_n(&*accumulator, &*line, read_count);
+		if (ft_strlen(*accumulator) == 0)
+			freethis(&*accumulator, "");
+	}
+	else
+	{
+		if (!ft_strlen(*accumulator) && read_count == 0)
+			return (0);
+		*line = ft_strdup(*accumulator);
+		freethis(&*accumulator, NULL);
+	}
+	return (1);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*accumulator = "";
+	char		*line;
+	int			read_count;
+
+	if (accumulator == NULL || fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	read_count = BUFFER_SIZE;
+	while (read_count > 0 && !ft_strchr(accumulator, '\n'))
+	{
+		read_count = feed_accumulator(&accumulator, fd);
+		if (read_count < 0)
+			return (NULL);
+	}
+	if (!close_gnl(&accumulator, &line, read_count))
+		return (NULL);
+	return (line);
+}
